0f0122b7dcdec48a00c1b5bd891edc117b52a180
fjlenz <fjlenz@gmail.com>
2026-07-19 19:32:31 +0200

    OpenAPIDefinition Server

--- backend/src/main/java/com/documentmanager/config/OpenApiConfig.java
+++ backend/src/main/java/com/documentmanager/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.documentmanager.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "This is the Server URL")})
 @SecuritySchemes({
     @SecurityScheme(
         name = "basicAuth",
